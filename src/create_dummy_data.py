1521808f475ef5dc1f1cec9e6fd48a08d1c975e4
prkhare <68290865+prkhare@users.noreply.github.com>
2025-12-31 14:53:15 +0530

    Update CSV output path for iris dataset

--- src/create_dummy_data.py
+++ src/create_dummy_data.py
@@ -4,7 +4,7 @@ from sklearn.datasets import load_iris
 def load_data():
     iris=load_iris(as_frame=True)
     df=iris.frame
-    df.to_csv("/workspaces/ML/data/raw/iris.csv",index=False)
+    df.to_csv("/workspaces/ML_DVC_DEMO/data/raw/iris.csv",index=False)
     print(df.head())
 
 if __name__=="__main__":
